63dfb7bb83bd6dfc1ed0a6d6f8f4b0c42c4a7bc8
Filip Wojciechowski <fwojciec@gmail.com>
2019-08-24 21:43:25 +0200

    code formatting changes

--- translations/config.ts
+++ translations/config.ts
@@ -1,5 +1,7 @@
 export const defaultLocale = 'en' as const
+
 export const locales = ['en', 'fr', 'pl'] as const
+
 export const languageNames = {
   en: 'English',
   fr: 'français',
--- translations/types.ts
+++ translations/types.ts
@@ -1,6 +1,7 @@
 import { locales } from './config'
 
 export type Locale = typeof locales[number]
+
 export type Strings = { [key in Locale]: { [key: string]: string } }
 
 export function isLocale(locale: string): locale is Locale {
