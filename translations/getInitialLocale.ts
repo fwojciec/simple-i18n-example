8e05159eb62cb1149c5aa14857124305a7841d66
Filip Wojciechowski <fwojciec@gmail.com>
2019-08-24 22:07:50 +0200

    simplify code

--- translations/getInitialLocale.ts
+++ translations/getInitialLocale.ts
@@ -2,18 +2,15 @@ import { defaultLocale } from './config'
 import { Locale, isLocale } from './types'
 
 export function getInitialLocale(): Locale {
-  // first check the localSetting, in case the preference was previously set
   const localSetting = localStorage.getItem('locale')
   if (localSetting && isLocale(localSetting)) {
     return localSetting
   }
 
-  // if that doesn't work, try setting based on the browser language setting
-  const browserSetting = navigator.language.split('-')[0]
+  const [browserSetting] = navigator.language.split('-')
   if (isLocale(browserSetting)) {
     return browserSetting
   }
 
-  // return the default otherwise
   return defaultLocale
 }
