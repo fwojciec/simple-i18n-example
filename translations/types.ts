49b6be2b715190ffc714857d713b252e049116e6
Filip Wojciechowski <fwojciec@gmail.com>
2019-08-24 21:55:30 +0200

    better variable names

--- translations/types.ts
+++ translations/types.ts
@@ -4,6 +4,6 @@ export type Locale = typeof locales[number]
 
 export type Strings = { [key in Locale]: { [key: string]: string } }
 
-export function isLocale(locale: string): locale is Locale {
-  return locales.some(loc => loc == locale)
+export function isLocale(tested: string): tested is Locale {
+  return locales.some(locale => locale === tested)
 }
