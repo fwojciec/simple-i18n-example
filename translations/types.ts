e1a50528f08b33295ce9e1f1431c0c1c43348bd1
Filip Wojciechowski <fwojciec@gmail.com>
2019-08-25 19:49:40 +0200

    code formatting

--- translations/types.ts
+++ translations/types.ts
@@ -2,7 +2,11 @@ import { locales } from './config'
 
 export type Locale = typeof locales[number]
 
-export type Strings = { [key in Locale]: { [key: string]: string } }
+export type Strings = {
+  [key in Locale]: {
+    [key: string]: string
+  }
+}
 
 export function isLocale(tested: string): tested is Locale {
   return locales.some(locale => locale === tested)
