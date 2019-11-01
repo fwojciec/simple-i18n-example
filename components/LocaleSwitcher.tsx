70400169c88524779b601c1d7367e671002dc471
billzhong <billzhong@gmail.com>
2019-10-31 22:41:37 -0230

    fix import next router

--- components/LocaleSwitcher.tsx
+++ components/LocaleSwitcher.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRouter } from 'next/dist/client/router'
+import { useRouter } from 'next/router'
 import { locales, languageNames } from '../translations/config'
 import { LocaleContext } from '../context/LocaleContext'
 
